78c79747f3e4bbec16b4aebfd119b42ecd4e8f1b
brian <balsamo@uchicago.edu>
2017-06-05 14:54:08 -0500

    Trying to abstract things in the right spot in order to not break the docker-compose build with the unifier and load balancers, while also allowing for a swarm build

--- setup.sh
+++ setup.sh
@@ -2,18 +2,23 @@
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $DIR/.env
-export UNIFIER_EXTERNAL_PORT UNIFIER_INTERNAL_PORT ARCHSTOR_INTERNAL_PORT QREMIS_API_INTERNAL_PORT ACC_IDNEST_INTERNAL_PORT DEAD_SIMPLE_INTERFACE_INTERNAL_PORT UNIFIER_HOST REC_API_INTERNAL_PORT
 
+# Clone the repos into this dir
+# If these error because they already exist thats fine, 
+# assume we just want to refresh our configs
+git clone https://github.com/bnbalsamo/archstor.git
+git clone https://github.com/uchicago-library/idnest.git
+git clone https://github.com/bnbalsamo/qremis_api.git
+git clone https://github.com/bnbalsamo/microservice_repository_dead_simple_interface.git
+git clone https://github.com/bnbalsamo/demo_records_api.git
+
+# Handle the configs
+source $DIR/.env
+export UNIFIER_EXTERNAL_PORT UNIFIER_INTERNAL_PORT ARCHSTOR_INTERNAL_PORT QREMIS_API_INTERNAL_PORT ACC_IDNEST_INTERNAL_PORT DEAD_SIMPLE_INTERFACE_INTERNAL_PORT UNIFIER_HOST REC_API_INTERNAL_PORT EXTERNAL_ARCHSTOR_URL EXTERNAL_QREMIS_API_URL
 for x in $DIR/configs/*; do envsubst < $x > `echo $x | rev | cut -c 10- | rev`.py; done
-git clone https://github.com/bnbalsamo/archstor.git && \
-    cp ./configs/archstor_conf.py archstor/config.py
-git clone https://github.com/uchicago-library/idnest.git && \
-    cp ./configs/acc_idnest_conf.py idnest/config.py
-git clone https://github.com/bnbalsamo/qremis_api.git && \
-    cp ./configs/qremis_api_conf.py qremis_api/config.py
-git clone https://github.com/bnbalsamo/microservice_repository_dead_simple_interface.git && \
-    cp ./configs/dead_simple_interface_conf.py microservice_repository_dead_simple_interface/config.py
-git clone https://github.com/bnbalsamo/demo_records_api.git && \
-    cp ./configs/demo_records_api_conf.py demo_records_api/config.py
+cp ./configs/archstor_conf.py archstor/config.py
+cp ./configs/qremis_api_conf.py qremis_api/config.py
+cp ./configs/dead_simple_interface_conf.py microservice_repository_dead_simple_interface/config.py
+cp ./configs/dead_simple_interface_conf.py microservice_repository_dead_simple_interface/config.py
+cp ./configs/demo_records_api_conf.py demo_records_api/config.py
 for x in $DIR/configs/*.template; do rm `echo $x | rev | cut -c 10- | rev`.py; done
